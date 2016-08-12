21c0673084303510e135da58a40303f14abd25c6
supermaro84 <mpierzchala84@gmail.com>
2016-08-12 15:42:17 +0200

    updated bash scripts

--- src/build/convert_all.sh
+++ src/build/convert_all.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 # since Bash v4
-FILES=/home/marek/pointclouds/*
+FILES=/home/marek/throttled_copy/*
+count=0
 for f in $FILES
 do
-echo "processing:  $f"
-/home/marek/marek_git/cylinder_trees/src/build/pcd_write_test $f
+((count++))
+if ((count < 10));
+then
+mv $f /home/marek/throttled_copy/scan00$count
+printf '%d' "$count"
+/home/marek/repos/vlp_pcl/src/build/pcd_write_test /home/marek/throttled_copy/scan00$count
+rm /home/marek/throttled_copy/scan00$count
+elif ((count > 9 && count < 100));
+then
+mv $f /home/marek/throttled_copy/scan0$count
+printf '%d' "$count"
+/home/marek/repos/vlp_pcl/src/build/pcd_write_test /home/marek/throttled_copy/scan0$count
+rm /home/marek/throttled_copy/scan0$count
+else
+mv $f /home/marek/throttled_copy/scan$count
+printf '%d' "$count"
+/home/marek/repos/vlp_pcl/src/build/pcd_write_test /home/marek/throttled_copy/scan$count
+rm /home/marek/throttled_copy/scan$count
+fi
 done
+
